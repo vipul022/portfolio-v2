5992092afd8bd1a0bfdd909d874d82de2d013ed9
vipul022 <vipul.seth022@gmail.com>
2021-02-18 17:18:05 +1100

    added basic html for about me

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.scss";
 import Header from "./components/Header";
+import AboutMe from "./components/AboutMe";
 function App() {
   return (
     <div>
       <Header />
+      <AboutMe />
     </div>
   );
 }
---
+++ src/components/AboutMe.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from "@material-ui/core";
+
+const AboutMe = () => {
+  return (
+    <Container section className="section-about">
+      <div className="u-center-text">
+        <h1 className="heading-secondary">About Me</h1>
+      </div>
+    </Container>
+  );
+};
+
+export default AboutMe;
