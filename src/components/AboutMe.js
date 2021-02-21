ad3ef3072dc68223d0c792110f56dabcd807bb6d
vipul022 <vipul.seth022@gmail.com>
2021-02-21 18:44:56 +1100

    added animation to about me section with react-reveal

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container, Grid } from "@material-ui/core";
+import { Fade } from "react-reveal";
 
 const AboutMe = () => {
   return (
@@ -7,30 +8,39 @@ const AboutMe = () => {
       <Container maxWidth={"md"}>
         <Grid container spacing={3}>
           <Grid item xs={12}>
-            <div className="u-center-text u-margin-bottom-big">
-              <h1 id="about-me" className="heading-secondary">About Me</h1>
-            </div>
-            <p class="text">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis sed
-              animi quod! A obcaecati delectus ipsa excepturi asperiores
-              exercitationem eos labore fugit sint! Nesciunt quam corporis
-              debitis? Sint ipsam tenetur laudantium magnam. Tempora recusandae,
-              ipsum a dolores error similique doloremque.
-            </p>
-            <p class="text">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis sed
-              animi quod! A obcaecati delectus ipsa excepturi asperiores
-              exercitationem eos labore fugit sint! Nesciunt quam corporis
-              debitis? Sint ipsam tenetur laudantium magnam. Tempora recusandae,
-              ipsum a dolores error similique doloremque.
-            </p>
-            <p class="text">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis sed
-              animi quod! A obcaecati delectus ipsa excepturi asperiores
-              exercitationem eos labore fugit sint! Nesciunt quam corporis
-              debitis? Sint ipsam tenetur laudantium magnam. Tempora recusandae,
-              ipsum a dolores error similique doloremque.
-            </p>
+            <Fade delay={300}>
+              <div className="u-center-text u-margin-bottom-big">
+                <h1 id="about-me" className="heading-secondary">
+                  About Me
+                </h1>
+              </div>
+            </Fade>
+
+            <Fade delay={600}>
+              <div>
+                <p class="text">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
+                  sed animi quod! A obcaecati delectus ipsa excepturi asperiores
+                  exercitationem eos labore fugit sint! Nesciunt quam corporis
+                  debitis? Sint ipsam tenetur laudantium magnam. Tempora
+                  recusandae, ipsum a dolores error similique doloremque.
+                </p>
+                <p class="text">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
+                  sed animi quod! A obcaecati delectus ipsa excepturi asperiores
+                  exercitationem eos labore fugit sint! Nesciunt quam corporis
+                  debitis? Sint ipsam tenetur laudantium magnam. Tempora
+                  recusandae, ipsum a dolores error similique doloremque.
+                </p>
+                <p class="text">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
+                  sed animi quod! A obcaecati delectus ipsa excepturi asperiores
+                  exercitationem eos labore fugit sint! Nesciunt quam corporis
+                  debitis? Sint ipsam tenetur laudantium magnam. Tempora
+                  recusandae, ipsum a dolores error similique doloremque.
+                </p>
+              </div>
+            </Fade>
           </Grid>
         </Grid>
       </Container>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { Container } from "@material-ui/core";
+import { Fade } from "react-reveal";
 
 const Header = () => {
   return (
     <header className="header">
       <Container className="header__text-box">
+        {/* <Fade delay={1000}> */}
+        {/* <> */}
         <h1 className="heading-primary">
           <span className="heading-primary--main">VIPUL SETH</span>
           {/* Hi, my name is Vipul */}
@@ -15,6 +18,8 @@ const Header = () => {
         <a href="#about-me" className="btn btn--white">
           know more
         </a>
+        {/* </> */}
+        {/* </Fade> */}
       </Container>
     </header>
   );
