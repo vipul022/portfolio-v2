ce117b1bb4f38dd26cd491f874693f88f6f2248c
vipul022 <vipul.seth022@gmail.com>
2021-02-23 01:26:23 +1100

    refactored the Projects component by reusing ProjectImage component for displaying all the images

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,6 +8,7 @@ import farmerSmartImage from "../images/farmer-smart.png";
 import weatherAppImage from "../images/weather-app.png";
 import portfolioImageV1 from "../images/portfolio-v1.png";
 import HeadingSecondary from "./HeadingSecondary";
+import ProjectImage from "./ProjectImage";
 
 const Projects = () => {
   return (
@@ -17,16 +18,10 @@ const Projects = () => {
         <div className="u-padding">
           <Grid container spacing={6} className="u-margin-bottom-big">
             <Grid item xs={12} sm={8}>
-              <Fade left distance={"4rem"} delay={500}>
-                <div className="card">
-                  <div className="card__side"></div>
-                  <img
-                    src={portfolioImage}
-                    alt="Portfolio version-2 website"
-                    className="card__img"
-                  />
-                </div>
-              </Fade>
+              <ProjectImage
+                ImageSrc={portfolioImage}
+                ImageAlt="Portfolio version-2 website"
+              />
             </Grid>
             <Grid item xs={12} sm={4}>
               <Fade right distance={"4rem"} delay={1000}>
@@ -42,68 +37,10 @@ const Projects = () => {
 
           <Grid container spacing={6} className="u-margin-bottom-big">
             <Grid item xs={12} sm={8}>
-              <Fade left distance={"4rem"} delay={500}>
-                <div className="card">
-                  <div className="card__side"></div>
-                  <img
-                    src={potteryClubImage}
-                    // src="../images/portfolio-v2.png"
-                    alt="South of the Rivers Potters Club website"
-                    className="card__img"
-                  />
-                </div>
-              </Fade>
-            </Grid>
-
-            <Grid item xs={12} sm={4}>
-              <Fade right distance={"4rem"} delay={1000}>
-                <h1>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Laboriosam nemo qui soluta sunt rerum quidem beatae
-                  praesentium odio vero quam quasi voluptates laudantium eveniet
-                  rem sint, molestiae voluptatibus possimus doloremque.
-                </h1>
-              </Fade>
-            </Grid>
-          </Grid>
-
-          <Grid container spacing={6} className="u-margin-bottom-big">
-            <Grid item xs={12} sm={8}>
-              <Fade left distance={"4rem"} delay={500}>
-                <div className="card">
-                  <div className="card__side"></div>
-                  <img
-                    src={expressChatImage}
-                    alt="Express Chat app"
-                    className="card__img"
-                  />
-                </div>
-              </Fade>
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Fade right distance={"4rem"} delay={1000}>
-                <h1>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Laboriosam nemo qui soluta sunt rerum quidem beatae
-                  praesentium odio vero quam quasi voluptates laudantium eveniet
-                  rem sint, molestiae voluptatibus possimus doloremque.
-                </h1>
-              </Fade>
-            </Grid>
-          </Grid>
-
-          <Grid container spacing={6} className="u-margin-bottom-big">
-            <Grid item xs={12} sm={8}>
-              <Fade left distance={"4rem"} delay={500}>
-                <div className="card">
-                  <div className="card__side"></div>
-                  <img
-                    src={weatherAppImage}
-                    alt="Weather app"
-                    className="card__img"
-                  />
-                </div>
-              </Fade>
+              <ProjectImage
+                ImageSrc={potteryClubImage}
+                ImageAlt="South of the Rivers Potters Club website"
+              />
             </Grid>
 
             <Grid item xs={12} sm={4}>
@@ -120,16 +57,10 @@ const Projects = () => {
 
           <Grid container spacing={6} className="u-margin-bottom-big">
             <Grid item xs={12} sm={8}>
-              <Fade left distance={"4rem"} delay={500}>
-                <div className="card">
-                  <div className="card__side"></div>
-                  <img
-                    src={farmerSmartImage}
-                    alt="FarmerSmart website"
-                    className="card__img"
-                  />
-                </div>
-              </Fade>
+              <ProjectImage
+                ImageSrc={expressChatImage}
+                ImageAlt="Express Chat ap"
+              />
             </Grid>
             <Grid item xs={12} sm={4}>
               <Fade right distance={"4rem"} delay={1000}>
@@ -145,16 +76,46 @@ const Projects = () => {
 
           <Grid container spacing={6} className="u-margin-bottom-big">
             <Grid item xs={12} sm={8}>
-              <Fade left distance={"4rem"} delay={500}>
-                <div className="card">
-                  <div className="card__side"></div>
-                  <img
-                    src={portfolioImageV1}
-                    alt="Portfolio version-1 website"
-                    className="card__img"
-                  />
-                </div>
+              <ProjectImage ImageSrc={weatherAppImage} ImageAlt="Weather app" />
+            </Grid>
+
+            <Grid item xs={12} sm={4}>
+              <Fade right distance={"4rem"} delay={1000}>
+                <h1>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Laboriosam nemo qui soluta sunt rerum quidem beatae
+                  praesentium odio vero quam quasi voluptates laudantium eveniet
+                  rem sint, molestiae voluptatibus possimus doloremque.
+                </h1>
               </Fade>
+            </Grid>
+          </Grid>
+
+          <Grid container spacing={6} className="u-margin-bottom-big">
+            <Grid item xs={12} sm={8}>
+              <ProjectImage
+                ImageSrc={farmerSmartImage}
+                ImageAlt="FarmerSmart website"
+              />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Fade right distance={"4rem"} delay={1000}>
+                <h1>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Laboriosam nemo qui soluta sunt rerum quidem beatae
+                  praesentium odio vero quam quasi voluptates laudantium eveniet
+                  rem sint, molestiae voluptatibus possimus doloremque.
+                </h1>
+              </Fade>
+            </Grid>
+          </Grid>
+
+          <Grid container spacing={6} className="u-margin-bottom-big">
+            <Grid item xs={12} sm={8}>
+              <ProjectImage
+                ImageSrc={portfolioImageV1}
+                ImageAlt="Portfolio version-1 website"
+              />
             </Grid>
 
             <Grid item xs={12} sm={4}>
