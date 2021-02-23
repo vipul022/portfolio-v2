db724c878caab15b64c2b3913852f6307a2f9b9f
vipul022 <vipul.seth022@gmail.com>
2021-02-23 22:27:17 +1100

    changed the layout of projects to make it responsive for ipad view

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -19,25 +19,25 @@ const Projects = () => {
         <HeadingSecondary>projects</HeadingSecondary>
         <div className="u-padding">
           <Grid container spacing={6} className="u-margin-bottom-big">
-            <Grid item xs={12} sm={8}>
+            <Grid item xs={12} md={7} sm={12}>
               <ProjectImage
                 ImageSrc={portfolioImage}
                 ImageAlt="Portfolio version-2 website"
               />
             </Grid>
-            <Grid item xs={12} sm={4}>
+            <Grid item xs={12} md={5} sm={12}>
               <Fade right distance={"4rem"} delay={1000}>
                 <Paper elevation={3} className="card-project-info">
-                  <HeadingTertiary>Portfolio v-2</HeadingTertiary>
+                  <HeadingTertiary>Portfolio Version-2</HeadingTertiary>
                   <ProjectDescription>
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                     Quia, ipsam praesentium facilis atque, voluptate, eius
                     repellendus soluta iure corrupti veniam delectus! Soluta,
-                    quia omnis. Doloribus eos, tempora doloremque sequi
-                    praesentium mollitia dolorem blanditiis ipsum,
+                    repellendus soluta iure corrupti veniam delectus! Soluta,
+                    repellendus soluta iure corrupti veniam delectus! Soluta,
                   </ProjectDescription>
 
-                  <Grid container spacing={3}>
+                  <Grid container spacing={6}>
                     <Grid item xs={4} className="icon">
                       <i class="devicon-react-original-wordmark colored"></i>
                     </Grid>
@@ -54,34 +54,63 @@ const Projects = () => {
           </Grid>
 
           <Grid container spacing={6} className="u-margin-bottom-big">
-            <Grid item xs={12} sm={8}>
+            <Grid item xs={12} md={7} sm={12}>
               <ProjectImage
                 ImageSrc={potteryClubImage}
                 ImageAlt="South of the Rivers Potters Club website"
               />
             </Grid>
 
-            <Grid item xs={12} sm={4}>
-              <Fade right distance={"4rem"} delay={1000}>
-                <p className="text-secondary">
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Quia, ipsam praesentium facilis atque, voluptate, eius
-                  repellendus soluta iure corrupti veniam delectus! Soluta, quia
-                  omnis. Doloribus eos, tempora doloremque sequi praesentium
-                  mollitia dolorem blanditiis ipsum,
-                </p>
+            <Grid item xs={12} md={5} sm={12}>
+              <Fade
+                right
+                distance={"4rem"}
+                delay={1000}
+                className="card-project-image"
+              >
+                <Paper elevation={3} className="card-project-info">
+                  <HeadingTertiary>MERN Stack App</HeadingTertiary>
+                  <ProjectDescription>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Quia,
+                  </ProjectDescription>
+
+                  <Grid container spacing={6}>
+                    <Grid item xs={4} className="icon">
+                      <i class="devicon-mongodb-plain-wordmark colored"></i>
+                    </Grid>
+                    <Grid item item xs={4} className="icon">
+                      <i class="devicon-express-original colored"></i>
+                    </Grid>
+                    <Grid item xs={4} className="icon">
+                      <i class="devicon-react-original-wordmark colored"></i>
+                    </Grid>
+                  </Grid>
+
+                  <Grid container spacing={6}>
+                    <Grid item xs={4} className="icon">
+                      <i class="devicon-nodejs-plain-wordmark colored"></i>
+                    </Grid>
+                    <Grid item xs={4} className="icon">
+                      <i class="devicon-amazonwebservices-plain-wordmark colored"></i>
+                    </Grid>
+                    <Grid item xs={4} className="icon">
+                      <i class="devicon-sass-original colored"></i>
+                    </Grid>
+                  </Grid>
+                </Paper>
               </Fade>
             </Grid>
           </Grid>
 
           <Grid container spacing={6} className="u-margin-bottom-big">
-            <Grid item xs={12} sm={8}>
+            <Grid item xs={12} md={7} sm={12}>
               <ProjectImage
                 ImageSrc={expressChatImage}
                 ImageAlt="Express Chat ap"
               />
             </Grid>
-            <Grid item xs={12} sm={4}>
+            <Grid item xs={12} md={5} sm={12}>
               <Fade right distance={"4rem"} delay={1000}>
                 <h1>
                   Lorem ipsum dolor sit, amet consectetur adipisicing elit.
@@ -94,11 +123,11 @@ const Projects = () => {
           </Grid>
 
           <Grid container spacing={6} className="u-margin-bottom-big">
-            <Grid item xs={12} sm={8}>
+            <Grid item item xs={12} md={7} sm={12}>
               <ProjectImage ImageSrc={weatherAppImage} ImageAlt="Weather app" />
             </Grid>
 
-            <Grid item xs={12} sm={4}>
+            <Grid item item xs={12} md={5} sm={12}>
               <Fade right distance={"4rem"} delay={1000}>
                 <h1>
                   Lorem ipsum dolor sit, amet consectetur adipisicing elit.
@@ -111,13 +140,13 @@ const Projects = () => {
           </Grid>
 
           <Grid container spacing={6} className="u-margin-bottom-big">
-            <Grid item xs={12} sm={8}>
+            <Grid item item xs={12} md={7} sm={12}>
               <ProjectImage
                 ImageSrc={farmerSmartImage}
                 ImageAlt="FarmerSmart website"
               />
             </Grid>
-            <Grid item xs={12} sm={4}>
+            <Grid item item xs={12} md={5} sm={12}>
               <Fade right distance={"4rem"} delay={1000}>
                 <h1>
                   Lorem ipsum dolor sit, amet consectetur adipisicing elit.
@@ -130,14 +159,14 @@ const Projects = () => {
           </Grid>
 
           <Grid container spacing={6} className="u-margin-bottom-big">
-            <Grid item xs={12} sm={8}>
+            <Grid item item xs={12} md={7} sm={12}>
               <ProjectImage
                 ImageSrc={portfolioImageV1}
                 ImageAlt="Portfolio version-1 website"
               />
             </Grid>
 
-            <Grid item xs={12} sm={4}>
+            <Grid item item xs={12} md={5} sm={12}>
               <Fade right distance={"4rem"} delay={1000}>
                 <h1>
                   Lorem ipsum dolor sit, amet consectetur adipisicing elit.
