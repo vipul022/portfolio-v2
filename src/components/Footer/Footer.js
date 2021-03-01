d7b9f22ccddea8f14911fe9cd924f4032c5d1191
vipul022 <vipul.seth022@gmail.com>
2021-03-01 21:02:58 +1100

    created SocialLink component to render all the links and make the code dry

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,20 +1,46 @@
 import React from "react";
+import SocialLink from "./SocialLink";
 import { Container, Grid } from "@material-ui/core";
-
 import { IoChevronUpSharp } from "react-icons/io5";
 import { FaLinkedin, FaGithub, FaPhone } from "react-icons/fa";
 import { IoMailOutline } from "react-icons/io5";
 
+const SocialLinksData = [
+  {
+    link: "https://www.linkedin.com/in/vipul-seth-a06730184/",
+    icon: <FaLinkedin />,
+  },
+  {
+    link: "https://github.com/vipul022",
+    icon: <FaGithub />,
+  },
+  {
+    link: "mailto: vipul.seth022@gmail.com",
+    icon: <IoMailOutline />,
+  },
+  {
+    link: "tel: +61473837804",
+    icon: <FaPhone />,
+  },
+];
 const Footer = () => {
+  const content = SocialLinksData.map((s, index) => (
+    <Grid item className="footer__social-link">
+      <SocialLink data={s} key={index} />
+    </Grid>
+  ));
   return (
     <footer className="footer">
       <Container>
         <Grid container>
+          {/* ! dummy gid container to adjust the layout */}
           <Grid container xs></Grid>
 
           <Grid container xs={10} direction="column" alignItems="center">
             <Grid item className="footer__back-to-top">
-              <IoChevronUpSharp />
+              <a href="#header" className="footer__back-to-top">
+                <IoChevronUpSharp />
+              </a>
             </Grid>
             <Grid item>
               <h1 className="footer__heading">REACH OUT</h1>
@@ -23,38 +49,13 @@ const Footer = () => {
             <hr class="footer__divider"></hr>
 
             <Grid container direction="row" justify="center">
-              <Grid item className="footer__social-link">
-                <a
-                  href="https://www.linkedin.com/in/vipul-seth-a06730184/"
-                  target="_blank"
-                >
-                  <FaLinkedin />
-                </a>
-              </Grid>
-              <Grid item className="footer__social-link">
-                <a href="https://github.com/vipul022" target="_blank">
-                  <FaGithub />
-                </a>
-              </Grid>
-
-              <Grid item className="footer__social-link">
-                <a href="mailto: vipul.seth022@gmail.com">
-                  <IoMailOutline />
-                </a>
-              </Grid>
-              <Grid item className="footer__social-link">
-                <a href="tel: +61473837804">
-                  <FaPhone />
-                </a>
-              </Grid>
+              {content}
             </Grid>
             <Grid item className="copyright">
-                <p className="copyright__text">
-                 Copyright © Vipul Seth 2021
-                </p>
-              </Grid>
+              <p className="copyright__text">Copyright © Vipul Seth 2021</p>
+            </Grid>
           </Grid>
-
+          {/* ! dummy gid container to adjust the layout */}
           <Grid container xs></Grid>
         </Grid>
       </Container>
