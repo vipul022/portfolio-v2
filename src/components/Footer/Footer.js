3951f306ae701aa0a019ad60b3771edcd11d839a
vipul022 <vipul.seth022@gmail.com>
2021-03-01 18:08:02 +1100

    adjusted styling of social links with Material UI grid

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,35 +9,52 @@ const Footer = () => {
   return (
     <footer className="footer">
       <Container>
-        <Grid container direction="column">
-          <Grid itemProp className="footer__back-to-top">
-            <IoChevronUpSharp />
-          </Grid>
-          <Grid item>
-            <h1 className="footer__heading">REACH OUT</h1>
-          </Grid>
+        <Grid container>
+          <Grid container xs></Grid>
 
-          <Grid
-            item
-            container
-            direction="row"
-            justify="center"
-            alignItems="center"
-            className="footer__social-links"
-          >
-            <Grid item className="footer__social-link">
-              <FaLinkedin />
+          <Grid container xs={8} direction={"column"} alignItems={"center"}>
+            <Grid item className="footer__back-to-top">
+              <IoChevronUpSharp />
             </Grid>
             <Grid item>
-              <FaGithub />
+              <h1 className="footer__heading">REACH OUT</h1>
             </Grid>
-            <Grid item>
-              <IoMailOutline />
-            </Grid>
-            <Grid item>
-              <FaPhone />
+
+            <hr class="footer__divider"></hr>
+
+            <Grid
+              container
+              direction="row"
+              justify="center"
+              className="footer__social-links"
+            >
+              <Grid item className="footer__social-link">
+                <a
+                  href="https://www.linkedin.com/in/vipul-seth-a06730184/"
+                  target="_blank"
+                >
+                  <FaLinkedin />
+                </a>
+              </Grid>
+              <Grid item className="footer__social-link">
+                <a href="https://github.com/vipul022" target="_blank">
+                  <FaGithub />
+                </a>
+              </Grid>
+
+              <Grid item className="footer__social-link">
+                <a href="mailto: vipul.seth022@gmail.com">
+                  <IoMailOutline />
+                </a>
+              </Grid>
+              <Grid item className="footer__social-link">
+                <a href="tel: +61473837804">
+                  <FaPhone />
+                </a>
+              </Grid>
             </Grid>
           </Grid>
+          <Grid container xs></Grid>
         </Grid>
       </Container>
     </footer>
