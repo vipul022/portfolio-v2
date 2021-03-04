bf5b028b473abf8b24f8d1357d8ea3a74c927205
vipul022 <vipul.seth022@gmail.com>
2021-03-04 22:36:35 +1100

    adjusted the projects description to allign with project image

--- src/components/Projects/ProjectItem.js
+++ src/components/Projects/ProjectItem.js
@@ -3,7 +3,7 @@ import HeadingTertiary from "../Headings/HeadingTertiary";
 import ProjectImage from "./ProjectImage";
 import ProjectDescription from "./ProjectDescription";
 import Icons from "./Icons";
-import ArrowRightAltIcon from "@material-ui/icons/ArrowRightAlt";
+
 import { Grid, Paper } from "@material-ui/core";
 import { Fade } from "react-reveal";
 
